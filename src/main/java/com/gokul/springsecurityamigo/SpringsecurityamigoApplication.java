9cebecd267b56338de188ecccf51e54efbd2789e
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-26 15:29:17 +0100

    initial commit

---
+++ src/main/java/com/gokul/springsecurityamigo/SpringsecurityamigoApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.springsecurityamigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityamigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityamigoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gokul/springsecurityamigo/SpringsecurityamigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.springsecurityamigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityamigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
